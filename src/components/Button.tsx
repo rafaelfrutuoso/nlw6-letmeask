eb8eb262f326811481086cea2340bce625b87a2a
rafaelfrutuoso <rafaelfrutuoso@live.com>
2021-06-22 18:39:53 -0300

    button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import {ButtonHTMLAttributes} from 'react'
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 export function Button(props:ButtonProps) {
